77bc74a1ed8d94122f814c9314e6b5a71fd639ff
Tomasz Kiljańczyk <39602955+Gunock@users.noreply.github.com>
2020-06-06 17:40:42 +0200

    Removed venv from repo

--- Python_server/TowerDefenseServer/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Szymon\Desktop\sem6_projekty\Secure-Tower-Defence\Python_server\TowerDefenseServer\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
